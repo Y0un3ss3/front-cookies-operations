82e7e3c3bc0e3ee06b4a47f59caa79c891e906af
unknown <you@example.com>
2024-08-05 08:21:23 +0200

    front v1

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+// Serve the HTML page
+app.get('/cookies', (req, res) => {
+    res.sendFile(__dirname + '/cookies.html');
+});
+
+app.get('/cors', (req, res) => {
+    res.sendFile(__dirname + '/cors.html');
+});
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
